79db1d328acea4b53a2dbc8249c1315684e8accf
Avanrethus <kazuhi13@gmail.com>
2016-10-29 19:17:27 +0200

    Added Bank (Singleton)

---
+++ Kasyno/src/kasyno/Bank/Bank.java
@@ -0,0 +1,32 @@
+package kasyno.Bank;
+
+public class Bank {
+    private static Bank inst;
+    int status = 2000;
+    
+    public Bank(){
+        
+    }
+    
+    public static Bank inst(){
+        if(inst == null){
+            inst = new Bank();
+        }
+        return inst;
+    }
+    
+    public int BankStatus(){
+        System.out.println(status+"\n");
+        return status;
+    }
+    
+    public int bankStatusPlus (int plus){
+        status = status + plus;
+        return status;
+    }
+    
+    public int BankStatusMinus(int minus){
+        status = status-minus;
+        return status;
+    }
+}
